30cdf16701085ebfdcf4ede7d03ff4c50e00ccb0
Georg Burgstaller <georg.burgstaller@gmx.at>
2019-08-26 20:31:37 +0200

    added clubsService to make controller easier testable

--- src/controllers/clubs/clubs.controller.ts
+++ src/controllers/clubs/clubs.controller.ts
@@ -7,17 +7,16 @@ import { IAuthenticatedRequest } from "../../interfaces/requests.interface";
 import { IController } from "../../interfaces/controller.interface";
 import { IClub } from "./clubs.interfaces";
 
-import Club, { ClubModel } from "./clubs.model";
+import Club from "./clubs.model";
 import ClubDto from "./clubs.dto";
 
-import { ClubNotFoundException } from "./clubs.exceptions";
 import { ClubsService } from "./clubs.service";
 
 
 export class ClubsController implements IController {
     public path = "/clubs";
     public router = Router();
-    private clubsSerivce = new ClubsService();
+    private clubsService = new ClubsService();
 
     constructor() {
         this.initializeRoutes();
@@ -35,7 +34,7 @@ export class ClubsController implements IController {
 
     private getClubs = async (req: Request, res: Response, next: NextFunction) => {
         try {
-            const clubs = await this.clubsSerivce.getClubs();
+            const clubs = await this.clubsService.getClubs();
 
             if (clubs) {
                 res.send(clubs.reduce((clubMap, club: IClub) => {
@@ -53,16 +52,18 @@ export class ClubsController implements IController {
 
     private getClubById = async (req: Request, res: Response, next: NextFunction) => {
         try {
-            const club = await this.clubsSerivce.getClubById(req.params.id);
+            const club = await this.clubsService.getClubById(req.params.id);
 
-            res.send(club);
+            if (club) {
+                res.send(club);
+            }
         }
         catch (err) {
             next(err);
         }
     }
 
-    private createClub = async (req: IAuthenticatedRequest, res: Response) => {
+    private createClub = async (req: IAuthenticatedRequest, res: Response, next: NextFunction) => {
         try {
             const club = new Club({
                 name: req.body.name,
@@ -71,49 +72,49 @@ export class ClubsController implements IController {
                 type: req.body.type
             });
 
-            club
-                .save()
-                .then(() => {
-                    return res.send(JSON.stringify(club));
-                })
-                .catch((err) => {
-                    return res.send(err);
-                });
+            const createResponse = await this.clubsService.createClub(club);
+
+            if (createResponse) {
+                res.send(createResponse);
+            }
         }
-        catch (error) {
-            res.status(500).send(error.toString());
+        catch (err) {
+            next(err);
         }
     }
 
     private updateClub = async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const club: IClub = {
+                _id: req.params.id,
+                name: req.body.name,
+                category: req.body.category,
+                managers: req.body.managers,
+                type: req.body.type
+            };
 
-        const updatedClub: IClub = {
-            _id: req.params.id,
-            name: req.body.name,
-            category: req.body.category,
-            managers: req.body.managers,
-            type: req.body.type
-        };
+            const updateClubResponse = await this.clubsService.updateClub(club);
 
-        Club
-            .findByIdAndUpdate(
-                req.params.id,
-                updatedClub,
-                { new: true }
-            )
-            .then((club: ClubModel | null) => {
-                res.send(club);
-            })
-            .catch(err => next(new ClubNotFoundException(req.params.id)));
+            if (updateClubResponse) {
+                res.send(updateClubResponse);
+            }
+        }
+        catch (err) {
+            next(err);
+        }
+
     }
 
     private deleteClub = async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const delteClubResponse = await this.clubsService.deleteClub(req.params.id);
 
-        Club
-            .findByIdAndDelete(req.params.id)
-            .then((club: ClubModel | null) => {
-                res.send(club);
-            })
-            .catch(err => next(new ClubNotFoundException(req.params.id)));
+            if (delteClubResponse) {
+                res.send(delteClubResponse);
+            }
+        }
+        catch (err) {
+            next(err);
+        }
     }
 }
--- src/controllers/clubs/clubs.service.ts
+++ src/controllers/clubs/clubs.service.ts
@@ -1,5 +1,7 @@
+import { IClub } from "./clubs.interfaces";
 import Club, { ClubModel } from "./clubs.model";
 import { ClubNotFoundException } from "./clubs.exceptions";
+
 
 export class ClubsService {
 
@@ -16,12 +18,61 @@ export class ClubsService {
         }
     }
 
-    public async getClubById(id) {
+    public async getClubById(id: string) {
         try {
             const club = await Club.findById(id);
 
             if (club) {
                 return club;
+            }
+            else {
+                throw new ClubNotFoundException(id);
+            }
+        }
+        catch (err) {
+            throw new ClubNotFoundException(id);
+        }
+    }
+
+    public async createClub(club: ClubModel) {
+        try {
+            const persistedClub = await club.save();
+
+            if (persistedClub) {
+                return persistedClub;
+            }
+        }
+        catch (err) {
+            throw err;
+        }
+    }
+
+    public async updateClub(club: IClub) {
+        try {
+            const updatedClub = await Club.findByIdAndUpdate(
+                club._id,
+                club,
+                { new: true }
+            );
+
+            if (updatedClub) {
+                return updatedClub;
+            }
+            else {
+                throw new ClubNotFoundException(club._id);
+            }
+        }
+        catch (err) {
+            throw new ClubNotFoundException(club._id);
+        }
+    }
+
+    public async deleteClub(id: string) {
+        try {
+            const deletedClub = await Club.findByIdAndDelete(id);
+
+            if (deletedClub) {
+                return deletedClub;
             }
             else {
                 throw new ClubNotFoundException(id);
